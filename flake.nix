8ffd7a6ae671f248eea819161db2d0dce4539cc5
Sukant Hajra <dev.sukant@hajra.xyz>
2026-05-30 23:40:01 -0500

    Address deprecation: nixfmt

--- flake.nix
+++ flake.nix
@@ -112,7 +112,7 @@
                   nixpkgs.stable.gnutar
                   nixpkgs.stable.gzip
                   nixpkgs.stable.jq
-                  nixpkgs.stable.nixfmt-rfc-style
+                  nixpkgs.stable.nixfmt
                   nixpkgs.stable.tree
                 ];
                 pathExtras = [
