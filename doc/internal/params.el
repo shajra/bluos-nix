471248eb1af3ec934fc5404e18ee55fc860d567c
Sukant Hajra <dev.sukant@hajra.xyz>
2026-05-30 23:25:40 -0500

    Update dependencies

--- doc/internal/params.el
+++ doc/internal/params.el
@@ -12,7 +12,7 @@
  ;; run-target-long will always prefix "bin/"
  (nix-latest           . "2.34.7")
  (nix-stable           . "2.31.5")
- (nixos-latest         . "25.11")
+ (nixos-latest         . "26.05")
  (platforms            . "\n\
 - Linux on x86-64 machines\n\
 - MacOS on x86-64 machines\n\
